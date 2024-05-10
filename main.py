7baca04655d20cbcaddc2ceaa568130563057bf4
Juan25Garcia <juanpablogg2508@gmail.com>
2024-05-10 00:26:53 -0600

    sd

--- main.py
+++ main.py
@@ -16,8 +16,18 @@ print("Vector Y:", ydir)
 x_now = 0
 y_now = 0
 theta_now = 0
+x_now, y_now, theta_now = giro(x_now, y_now, theta_now, xdir[0], ydir[0])
+x_now, y_now, theta_now = despl(x_now, y_now, theta_now, xdir[0], ydir[0])
+i =1
 
-    
-for i in range(len(xdir)):
+for i in range(len(xdir)-1):
     x_now, y_now, theta_now = giro(x_now, y_now, theta_now, xdir[i], ydir[i])
+    while True:  # Sub-bucle que actúa como pausa
+        respuesta = input("Ingrese 'True' para continuar: ")
+        
+        if respuesta.strip().lower() == "true":  # Si recibe "True", sigue con el bucle principal
+            break  # Rompe el sub-bucle para volver al bucle principal
+        else:
+            print("Esperando la señal para continuar...")
+
     x_now, y_now, theta_now = despl(x_now, y_now, theta_now, xdir[i], ydir[i])
