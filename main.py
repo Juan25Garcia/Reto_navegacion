8649c335d0c90b90e523fb071d72891167286338
JP <juanpablogg2508@gmail.com>
2024-05-13 18:42:36 -0600

    Se han agregado las rutinas

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from robot_dife_giro import giro
 from matrices import matrix
 
 
+
+
 # Solicitar al usuario la entrada
 trayectoria_input = input("Ingrese las coordenadas como 'x,y; x,y; ...': ")
+#trayectoria_input = "0,5;5,5;5,0;0,0" #cuadrado
+#trayectoria_input = "0,5;2.5,5;2.5,0;0,0" #rectangulo
+#trayectoria_input = "-0.73,1;1.38,2.39;3.09,1.38;2.36,0;0,0" #pentagono
+#trayectoria_input = "-1,1;0,2;5,2;6,1;5,0;0,0" #Figura rara
+
 
 # Llamar a la función para obtener vectores X e Y
 xdir, ydir = matrix(trayectoria_input)
+
 
 # Imprimir los vectores resultantes
 print("Vector X:", xdir)
@@ -16,20 +24,17 @@ print("Vector Y:", ydir)
 x_now = 0
 y_now = 0
 theta_now = 0
-x_now, y_now, theta_now = giro(x_now, y_now, theta_now, xdir[0], ydir[0])
-x_now, y_now, theta_now = despl(x_now, y_now, theta_now, xdir[0], ydir[0])
-i =1
 
-for i in range(len(xdir)-1):
+
+for i in range(len(xdir)):
     x_now, y_now, theta_now = giro(x_now, y_now, theta_now, xdir[i], ydir[i])
-    while True:  # Sub-bucle que actúa como pausa
-        respuesta = input("Ingrese 'True' para continuar: ")
-        
-        if respuesta.strip().lower() == "true":  # Si recibe "True", sigue con el bucle principal
-            break  # Rompe el sub-bucle para volver al bucle principal
-        else:
-            print("Esperando la señal para continuar...")
-
     x_now, y_now, theta_now = despl(x_now, y_now, theta_now, xdir[i], ydir[i])
 
 #Mientras mande un true funciona el programa pero si mando un fa
+
+    #while True:  # Sub-bucle que actúa como pausa
+        #respuesta = input("Ingrese 'True' para continuar: ")
+        #if respuesta.strip().lower() == "true":  # Si recibe "True", sigue con el bucle principal
+            #break  # Rompe el sub-bucle para volver al bucle principal
+        #else:
+            #print("Esperando la señal para continuar...")
